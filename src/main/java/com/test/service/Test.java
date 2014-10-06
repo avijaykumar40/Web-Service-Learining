5fbadb900226d2b77d7b0dd81212fec9663dffb9
vijay <vijay@vijay-pc>
2014-10-06 17:38:00 +0530

    Initial Commit

---
+++ src/main/java/com/test/service/Test.java
@@ -0,0 +1,17 @@
+package com.test.service;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+// Servlet implementation class Test
+public class Test extends HttpServlet {
+       private static final long serialVersionUID = 1L;
+       @Override
+       protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, 
+		IOException {
+               PrintWriter p = resp.getWriter();
+               p.print("WORKING…………...");
+       }
+}
